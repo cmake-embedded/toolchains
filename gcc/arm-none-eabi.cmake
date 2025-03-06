5609f2d794f311e793c03fe75a9eea314f3eaa31
Canmor Lam <canmor.lam@gmail.com>
2025-03-06 20:34:35 +0800

    Add helper funtions

--- gcc/arm-none-eabi.cmake
+++ gcc/arm-none-eabi.cmake
@@ -1,3 +1,4 @@
+if(NOT ARM_NONE_EABI_CONFIGURED)
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
 
@@ -11,3 +12,34 @@ set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 set(CMAKE_OBJDUMP arm-none-eabi-objdump)
 set(SIZE arm-none-eabi-size)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+function(target_linker_script target script)
+    get_target_property(target_type ${target} TYPE)
+    if(target_type STREQUAL "EXECUTABLE")
+        target_link_options(${target} PRIVATE "LINKER:-T,${script}")
+        set_target_properties(${target} PROPERTIES LINK_DEPENDS ${script})
+    else()
+        target_link_options(${target} INTERFACE "LINKER:-T,${script}")
+        set_target_properties(${target} PROPERTIES INTERFACE_LINK_DEPENDS ${script})
+    endif()
+endfunction()
+
+function(target_print_memory_usage target)
+    target_link_options(${target} PRIVATE "LINKER:--print-memory-usage,-Map=$<TARGET_FILE:${target}>.map")
+endfunction()
+
+function(target_export_hex TARGET)
+add_custom_command(TARGET ${TARGET} POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${TARGET}> $<TARGET_FILE:${TARGET}>.hex
+    COMMAND ${CMAKE_COMMAND} -E echo "Exporting $<TARGET_FILE:${TARGET}>.hex"
+    )
+endfunction()
+
+function(target_export_bin TARGET)
+add_custom_command(TARGET ${TARGET} POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${TARGET}> $<TARGET_FILE:${TARGET}>.bin
+    COMMAND ${CMAKE_COMMAND} -E echo "Exporting $<TARGET_FILE:${TARGET}>.bin"
+    )
+endfunction()
+set(ARM_NONE_EABI_CONFIGURED TRUE)
+endif()
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Lijun Lin on 2022/10/26.
-//
+extern "C" void _exit(int) {
+    while (1);
+}
+
+#include <cmath>
+#include <array>
+
+std::array<double, 128> data;
+
+int main() {
+    for (unsigned int i = 1; i > 0; i++) {
+        data[i % data.size()] += std::sin(i);
+    }
+    return 0;
+}
